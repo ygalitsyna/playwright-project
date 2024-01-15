51fc6705c5b8d72e501bf50e6c2b9b195cbcc44c
ygalitsyna <ygalitsyna@solvd.com>
2024-01-15 15:59:47 +0100

    feat: add testApiSearch to test inequality of 2 videos using api

---
+++ tests/testApiSearch.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect, request } from '@playwright/test';
+import apiSearchIncognito from '../test-data/apiSearchIncognito.json';
+
+test('check inequality of the first videos on the youtube page using API', async ({request}) => {
+    const response = await request.post('/youtubei/v1/search?*', {
+        data: JSON.stringify(apiSearchIncognito)
+    })
+    expect(response.status()).toEqual(200)
+    const responseBody = await response.json()
+    const videoRenderersList = responseBody.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents.filter( render => render.videoRenderer)
+    expect(videoRenderersList[0].videoRenderer.title.runs[0].text).not.toEqual(videoRenderersList[1].videoRenderer.title.runs[0].text)
+    expect(videoRenderersList[0].videoRenderer.longBylineText.runs[0].text).not.toEqual(videoRenderersList[1].videoRenderer.longBylineText.runs[0].text)
+    expect(videoRenderersList[0].videoRenderer.detailedMetadataSnippets[0].snippetText.runs[0].text).not.toEqual(videoRenderersList[1].videoRenderer.detailedMetadataSnippets[0].snippetText.runs[0].text)
+    expect(videoRenderersList[0].videoRenderer.thumbnail.thumbnails[0].url).not.toEqual(videoRenderersList[1].videoRenderer.thumbnail.thumbnails[0].url)
+})
